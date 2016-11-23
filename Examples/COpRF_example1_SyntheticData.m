20ccd0a0c3b592380d3864eb61ccd4443e354e10
davesl <dave.slater15@gmail.com>
2016-11-23 17:46:39 +0100

    Add files via upload

--- Examples/COpRF_example1_SyntheticData.m
+++ Examples/COpRF_example1_SyntheticData.m
@@ -1,4 +1,4 @@
-%% CO-pRF synthetic data example
+%% CO-pRF Example1: synthetic data
 % In this script we will look at how to generate some simple synthetic data
 % and estimate pRF parameters using the CO-pRF toolbox.
 
@@ -29,8 +29,21 @@ SyntheticOptions.Min_expts = 0.25;      % minimum compressive exponent to model
 SyntheticOptions.Nexpts = 3;            % Number of exponent values to model
 [SyntheticData, SyntheticParams] = Build_pRF_dictionary(stimulus,'',TR,SyntheticOptions,1);
 
+% Ignore first 2 columns - these are added to dictionaries to correct for 
+% non mean-centred data
+SyntheticData = SyntheticData(:,3:end);
+SyntheticParams = SyntheticParams(3:end,:);
 
-%% 2. Create dense dictionary for model fitting
+%% 2. Add Gaussian white noise
+% We will add some Gaussian noise to each simulated timeseries
+
+snr=5; % Arbitrarily choise an SNR of 5:1
+for i=1:size(SyntheticData,2)
+    y = SyntheticData(:,i);
+    SyntheticData(:,i) = awgn(y,snr);
+end
+
+%% 3. Create dense dictionary for model fitting
 % Generating the dictionary can take approx. 5-10mins. However we only 
 % need to precomute the dictionary once for a given stimuli. For a study 
 % with many subjects who were each presented with the same stimuli the 
@@ -43,62 +56,113 @@ SyntheticOptions.Nexpts = 3;            % Number of exponent values to model
 % COpRF_options.Nexpts = 1;
 % COpRF_options.Min_expts = 1;
 
-% Generate the 
-[Dictionary, DictionaryParams] = Build_pRF_dictionary(stimulus,'',TR,COpRF_options,1);
+% Create a cell array containing the single data session with time along
+% second dimention
+data = {SyntheticData'};
+
+% Build the CO-pRF dictionary
+[Dictionary, DictionaryParams, COpRF_options] = Build_pRF_dictionary(stimulus,data,TR,COpRF_options,1);
 COpRF_options.D = Dictionary;
 COpRF_options.Dparams = DictionaryParams;
 
+% If we save COpRF_options the same options file and dictionary could be
+% loaded and used for other datasets using the same stimuli.
 save(fullfile(ExamplesDir,'Example_COpRF_options.mat'),'COpRF_options') 
-% To save time Example_COpRF_options.mat could now be loaded and used for step 3.
 
 
-%% 3. CO-pRF model fitting
+%% 4. CO-pRF model fitting
 % To estimate model parameters from the data we need to supply stimulus and
 % data variables. These should take the form of cell arrays with presented
 % stimuli and recorded timeseries in a separate cell for each session.
+% 
 % Stimuli should be Npixel x Npixels x Ntimepoints
 % Data should be Nvoxels x Ntimepoints
 
-data = {SyntheticData(:,3:end)}; % Create a cell array containing the single data session with time
-fitHRF = 0; % Use a cannonical HRF rather than estimating from the data
-
-% Begin the model CO-pRF fitting
+% Begin the CO-pRF model fitting
 [results, B, G] = Convex_pRF_fit_parallel(stimulus,data,TR,COpRF_options);
-
-
-%% 4. Plot estimated parameters vs. synthetic groundtruth
-% We will create some simple scatter plots to show the accuracy of the
-% CO-pRF model fit.
-
-vxs = results.vxs; % Indices for modeled timeseries in data
-
-% Groundtruth parameter values
-X_GT = SyntheticParams(vxs+2,1); % groundtruth x positions
-Y_GT = SyntheticParams(vxs+2,2); % groundtruth y positions
-Sigma_GT = SyntheticParams(vxs+2,3); % groundtruth rfsize positions
-Exponent_GT = SyntheticParams(vxs+2,5); % groundtruth exponent positions
-
-% Estimated parameter values
-X_fit = B(vxs,1); % estimated x positions
-Y_fit = B(vxs,2); % estimated y positions
-Sigma_fit = B(vxs,3); % estimated rfsize positions
-Exponent_fit = B(vxs,5); % estimated exponent positions
-
-% Create scatter plots of estimated vs. groundtruth
-figure;
-scatter(X_fit,X_GT)
-figure;
-scatter(Y_fit,Y_GT)
-figure;
-scatter(Sigma_fit,Sigma_GT)
-figure;
-scatter(Exponent_fit,Exponent_GT)
 
 
 %% 5. Convert from pixels to degrees
 % The COpRF toolbox estimates model parameters in a stimuli defined space.
 % However converting to visual angle is trivial.
 
+StimEcc = 9; % Assume stimuli extends to 9 degrees of eccentricity.
+StimRes=100; % Stimulus resolution (diameter)
+Cfactor = (StimEcc*2)/StimRes;
+
+results.rfsize_degrees = results.rfsize*Cfactor;
+results.ecc_degrees = results.ecc*Cfactor;
+results.xpos_degrees = results.xpos*Cfactor;
+results.ypos_degrees = results.ypos*Cfactor;
+
+
+%% 6. Plot estimated parameters vs. synthetic groundtruth
+% We will create some simple plots to show the accuracy of the CO-pRF 
+% model fit.
+
+% Define CSS-pRF model function (Kay et al., 2013)
+[~,xx,yy] = makegaussian2d(StimRes,2,2,2,2);
+modelfun = @(pp,ss) conv2run(posrect(pp(4)) * (ss*[vflatten(placematrix(zeros([StimRes StimRes]),makegaussian2d(StimRes,pp(1),pp(2),abs(pp(3)),abs(pp(3)),xx,yy,0,0) / (2*pi*abs(pp(3))^2))); 0]) .^ posrect(pp(5)),COpRF_options.hrf,ss(:,StimRes^2+1));
+% Create matrix to regress out low frequency drift in time-series
+tmatrix = Build_poly_regressors(data,TR,COpRF_options);
+% prepare stimuli
+SS=[];
+for ii=1:length(stimulus)
+    SS{ii} = squish(stimulus{ii},2)';  % frames x pixels
+    SS{ii} = [SS{ii} ii*ones(size(SS{ii},1),1)];  % add a dummy column to indicate run breaks
+end
+SS=catcell(1,SS);
+
+% Select 10 example timeseries
+N=10;
+I= [331   278    42   363   626   198   241   156   219   115];
+% I=randsample(size(data{1},2),N); % Or randomize selection
+
+% Plot data with estimated parameter predictions
+figure('Position',[100 80, 800, 900])
+for i=1:N
+    subplot(N,1,i)
+    % Plot synthetic data
+    Y1=tmatrix*data{1}(I(i),:)';
+    plot(Y1,'b')
+    hold on
+    % Plot prediction from parameter estimates
+    Y2 = tmatrix*modelfun(B(I(i),:),SS);
+    plot(Y2,'r','LineWidth',2)
+end
+
+% Plot the groundtruth (blue) and estimated (red) pRFs. 
+% pRFs with large receptive fields, extreme eccentricity and/or small 
+% compressive exponets are challenging to estimate and will match more 
+% poorly to the groundtruth than pRFs which overlap entirely with the 
+% stimulus area.
+figure('Position',[1100 200, 600, 600]); hold on; axis square; grid on;
+GT_xpos_degrees=(SyntheticParams(:,1)-(StimRes+1)/2)*Cfactor;
+GT_ypos_degrees=(SyntheticParams(:,2)-(StimRes+1)/2)*Cfactor;
+GT_rfsize_degrees=SyntheticParams(:,3)./sqrt(SyntheticParams(:,5))*Cfactor;
+ang=0:0.01:2*pi;
+xp=StimEcc*cos(ang);
+yp=StimEcc*sin(ang);
+plot(0+xp,0+yp,'k','LineWidth',2); % Stimulus extent
+for i=1:N
+    % Plot groundtruth
+    x=GT_xpos_degrees(I(i));
+    y=GT_ypos_degrees(I(i));
+    r=GT_rfsize_degrees(I(i));
+    scatter(x,y,10,'b','fill')
+    xp=r*cos(ang);
+    yp=r*sin(ang);
+    plot(x+xp,y+yp,'b');
+    % Plot estimate
+    x=results.xpos_degrees(I(i));
+    y=results.ypos_degrees(I(i));
+    r=results.rfsize_degrees(I(i));
+    scatter(x,y,10,'r','fill')
+    xp=r*cos(ang);
+    yp=r*sin(ang);
+    plot(x+xp,y+yp,'r');
+end
+xlim([-StimEcc StimEcc]); ylim([-StimEcc StimEcc]);
 
 %% References
 % Original pRF model: Dumoulin, S.O., Wandell, B. a., 2008. Population receptive field estimates in human visual cortex. Neuroimage 39, 647–660.
